7657fe0e07e8d5e618d7961e290ef1c0a7fb8bb0
Michael Hernandez <gammabeta010@gmail.com>
2020-03-06 21:57:42 +0100

    Finito Milestone 1.2 e 2.1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,43 @@
 $(document).ready(function() {
 
+    $(".new-message").focus(function() {
+        $(".send-reply .fa-paper-plane").toggleClass("fas fa-paper-plane fa fa-microphone");
+    })
+    $(".new-message").blur(function() {
+        $(".send-reply .fa-microphone").toggleClass("fas fa-paper-plane fa fa-microphone");
+    })
+
     var messageText;
     var newMessageObj;
 
     $(".new-message").keypress(function(event){
         if(event.keyCode == 13) {
-            insertMessage();
-
-
+            insertUserMessage();
+            setTimeout(insertFriendMessage, 1000);
         }
     })
+    $(".fa-paper-plane").click(function(event){
+            insertUserMessage();
+            setTimeout(insertFriendMessage, 1000);
+    })
 
-    function insertMessage() {
+
+    function insertUserMessage() {
         messageText = $(".new-message").val()
         $(".new-message").val('');
         newMessageObj = $(".template .wrapper").clone();
         newMessageObj.find(".messaggio").text(messageText);
         newMessageObj.addClass("user-side");
         newMessageObj.children(".chat").addClass("user-color");
+        $("#chat-container").append(newMessageObj);
+    }
+
+    function insertFriendMessage() {
+        messageText = "ok";
+        newMessageObj = $(".template .wrapper").clone();
+        newMessageObj.find(".messaggio").text(messageText);
+        newMessageObj.addClass("friend-side");
+        newMessageObj.children(".chat").addClass("friend-color");
         $("#chat-container").append(newMessageObj);
     }
 
