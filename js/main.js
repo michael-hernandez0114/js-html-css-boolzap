850b7f362f6050cf2c9cb29a9a27a91e9aaa5b8a
Michael Hernandez <gammabeta010@gmail.com>
2020-03-09 21:02:35 +0100

    Finito Milestone 3.1

--- js/main.js
+++ js/main.js
@@ -27,17 +27,19 @@ $(document).ready(function() {
         var user = $(this).data('conversation');
         $("#chat-container .conversation").each(function(){
             if(user == $(this).data('conversation')) {
-                $("#chat-container .conversation").hide();
-                $(this).show();
+
+                //$("#chat-container .conversation").hide();
+                //$(this).show();
+                $("#chat-container .conversation").removeClass("active");
+                $(this).addClass("active");
             }
         });
+        // change friend card to selected card by click
         $(".friend-card").removeClass("active-friend");
         $(this).addClass("active-friend");
 
-    });
 
-    var messageText;
-    var newMessageObj;
+    });
 
     $(".new-message").keypress(function(event){
         if(event.keyCode == 13) {
@@ -52,22 +54,34 @@ $(document).ready(function() {
 
 
     function insertUserMessage() {
+        var messageText;
+        var newMessageObj;
+        var getConversationBlock;
+
         messageText = $(".new-message").val()
         $(".new-message").val('');
         newMessageObj = $(".template .wrapper").clone();
         newMessageObj.find(".messaggio").text(messageText);
         newMessageObj.addClass("user-side");
         newMessageObj.children(".chat").addClass("user-color");
-        $("#chat-container").append(newMessageObj);
+
+        $(".conversation.active").append(newMessageObj);
+
     }
 
     function insertFriendMessage() {
+        var messageText;
+        var newMessageObj;
+
         messageText = "ok";
         newMessageObj = $(".template .wrapper").clone();
         newMessageObj.find(".messaggio").text(messageText);
         newMessageObj.addClass("friend-side");
         newMessageObj.children(".chat").addClass("friend-color");
-        $("#chat-container").append(newMessageObj);
+
+        $(".conversation.active").append(newMessageObj);
+
+        //$("#chat-container").append(newMessageObj);
     }
 
 
